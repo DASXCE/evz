20e487d9ddfed9da2274da928d939412f197f5aa
DASXCE <goranrajta@gmail.com>
2013-06-15 13:11:25 +0200

    dodata statistika

--- src/main/java/fit/piris/evz/pages/Index.java
+++ src/main/java/fit/piris/evz/pages/Index.java
@@ -4,12 +4,15 @@ import java.util.Date;
 
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
 
 import fit.piris.evz.annotations.VeterinarAccess;
 import fit.piris.evz.annotations.VlasnikAccess;
+import fit.piris.evz.entities.gazdinstvo.Gazdinstvo;
 import fit.piris.evz.entities.users.User;
 import fit.piris.evz.entities.users.Veterinar;
 import fit.piris.evz.entities.users.Vlasnik;
+import fit.piris.evz.entities.zivotinje.Zivotinja;
 import fit.piris.evz.services.security.Authenticator;
 
 /**
@@ -22,6 +25,21 @@ public class Index {
 	@Inject
 	private Authenticator authenticator;
 
+	@Inject
+	private Session session;
+
+	@Property
+	private int brKorisnikaUSistemu = session.createCriteria(User.class).list().size();
+	
+	@Property
+	private int brGazdinstavaUSistemu = session.createCriteria(Gazdinstvo.class).list().size();
+	
+	@Property
+	private int brVeterinaraUSistemu = session.createCriteria(Veterinar.class).list().size();
+	
+	@Property
+	private int brZivotinjaUSistemu = session.createCriteria(Zivotinja.class).list().size();
+	
 	@Property
 	private Date danas = new Date();
 
